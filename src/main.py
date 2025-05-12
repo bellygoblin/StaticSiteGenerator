65a635107d9d2d725257f3ea3f8e74a9fee6ebf6
bellygoblin <brian@mitchellfamily.me>
2025-05-12 06:20:54 -0400

    HTML Node Setup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 
 def main():
-  text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-  print(text_node)
+  # text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+  # print(text_node)
+  html_node = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
+  print(html_node.props_to_html())
+  print(html_node)
 
 main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,19 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+  def test_eq(self):
+    node = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
+    node2 = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
+    self.assertEqual(node, node2)
+
+  def test_props_to_html(self):
+    node = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
+    self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
+
+  def test_repr(self):
+    node = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
+    self.assertEqual(repr(node), "HTMLNode(a, Google, None, {'href': 'https://google.com', 'target': '_blank'})")
+
+if __name__ == "__main___":
+  unittest.main()
