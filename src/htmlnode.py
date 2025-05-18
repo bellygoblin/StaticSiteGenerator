d15f58c80b36d6a7802e47039234d781ae3634d4
bellygoblin <brian@mitchellfamily.me>
2025-05-18 08:12:48 -0400

    Convert Textnode to HTML

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,9 +27,29 @@ class HTMLNode():
   def __repr__(self):
     return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
 
+
+
 class ParentNode(HTMLNode):
   def __init__(self, tag, children, props=None):
     super().__init__(tag, None, children, props)
+
+  def to_html(self):
+    if self.tag is None:
+      raise ValueError("Invalid HTML: no tag")
+    
+    if self.children is None:
+      raise ValueError("Invalid HTML: missing child item")
+    
+    children_html = ''
+
+    for child in self.children:
+      children_html += child.to_html()
+    
+    return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+  
+  def __repr__(self):
+    return f'ParentNode({self.tag}, {self.children}, {self.props})'
 
 class LeafNode(HTMLNode):
   def __init__(self, tag, value, props = None):
--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 from textnode import TextType, TextNode
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
-  # text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-  # print(text_node)
-  # html_node = HTMLNode("a", "Google", None, {"href": "https://google.com", "target": "_blank"})
-  # print(html_node.props_to_html())
-  # print(html_node)
-  leaf_node1 = LeafNode("p", "This is a paragraph of text.")
-  leaf_node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-  print(leaf_node1.to_html())
-  print(leaf_node2.to_html())
+  grandchild_node = LeafNode("b", "grandchild")
+  child_node = ParentNode("span", [grandchild_node])
+  parent_node = ParentNode("div", [child_node])
+
+  print(parent_node.to_html())
 
 main()
