17fc7ccddcde7e88dd9e43d97383d9510f0eb92c
bellygoblin <brian@mitchellfamily.me>
2025-05-26 12:28:02 -0400

    Inline Markdown Setup

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from inline_markdown import split_nodes_delimiter
 
 def main():
-  grandchild_node = LeafNode("b", "grandchild")
-  child_node = ParentNode("span", [grandchild_node])
-  parent_node = ParentNode("div", [child_node])
+  node = TextNode("This is text with a `code block` word", TextType.TEXT)
+  new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+  print(new_nodes)
 
-  print(parent_node.to_html())
+
 
 main()
